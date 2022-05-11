54493b8ae736cd093398c5a8129cef675c7b840a
Marco Antonio <marcosdesouza772@gmail.com>
2022-05-11 17:43:15 -0300

    update 2

--- Controlador.py
+++ Controlador.py
@@ -27,22 +27,42 @@ def continuar():  #Função para a opção de continuar
 
 
 # =================================================== Motorista =====================================================
-banco_Motorista={44555888990: {"CPF": 44555888990, "nome": "João", "habilitacao": "AB"}}
+banco_Motorista = {44555888990: {"CPF": 44555888990, "nome": "João", "habilitacao": "AB"},
+                   22366895108: {"CPF": 22366895108, "nome": "Paulo", "habilitacao": "B"}}
 
 
 def cadastro_Motorista():
+    print("=-=-Cadastro do Motorista=-=-=-")
+    print("=-" * 15)
     while True:
-        cpf = int(input("insira um CPF: "))
-        #criar uma função que verifique se o cpf ja existe
-        nome = input("Insira o nome do funcionario: ").title()
-        habilitacao = input("Insira a carteira de habilitação: ")
-        dic_Motorista = {"CPF": cpf, "nome": nome, "hablitacao": habilitacao}
-        banco_Motorista[cpf] = dic_Motorista
-        opcao= input("Deseja continuar")
-        if opcao != 1:
-            return
+        cpf = input("Insira um CPF com 11 dígitos: ").strip()
+        while not cpf.isdigit() or len(cpf) != 11:
+            cpf = input("Informação incorreta. Digite somente números ou um CPF válido (com 11 dígitos): ").strip()
+        cpf = int(cpf)
+        if not checagem(cpf):
+            nome = str(input("Insira o nome do funcionario: "))
+            while not nome.isalpha():
+                nome = input("Informação incorreta. Insira somente letras: ")
+            habilitacao = str(input(f"Insira a carteira de habilitação de {nome} (A, B ou AB): "))
+            while habilitacao not in "ABab":
+                habilitacao = str(input(f"Informação incorreta. Digite somente uma carteira A, B ou AB: "))
+            dic_Motorista = {"CPF": cpf, "nome": nome.strip().title(), "habilitacao": habilitacao.strip().upper()}
+            banco_Motorista[cpf] = dic_Motorista
+            if not continuar():
+                return
+            print("=-" * 15)
+        else:
+            for pessoa, motorista in banco_Motorista.items():
+                print(motorista)
+                if pessoa == cpf:
+                    print(f'O CPF {cpf} pertence ao motorista {motorista.get("nome")} e já está cadastrado.')
+                    break
+            if not continuar():
+                return
+            return cadastro_Motorista()
 
 
+def buscarMotorista():
 
 # ========================================= Veículo ===================================================================
 banco_Veiculo = {'2HBR220': {'placa': "2HBR220", 'tipo': "Carro", "motorista": 'João'},
@@ -58,14 +78,10 @@ def cadastrarVeiculo():
             print("informação incorreta.")
             placa = str(input("Insira a placa do seu veículo com 7 caracteres(ex: 2H6BR22) "
                               "e somente letras e números: ")).strip().upper()
-            if placa.isalnum() and len(placa) == 7:
-                break
         if not checagem(placa, 1):  # coloquei if not. porque ele só vai continuar se ele não existir (Que é False)
             tipo = str(input("Insira o tipo do seu veículo (moto ou carro): ")).strip().title()
             while tipo != "Moto" and tipo != "Carro":
                 tipo = str(input("Infomação incorreta. Digite somente moto ou carro: ")).strip().title()
-                if tipo == "Moto" or tipo == 'Carro':
-                    break
             veiculo = {"placa": placa, "tipo": tipo, "motorista":None}
             banco_Veiculo[placa] = veiculo
         else:
