e68253473f9c38af8b28ba39cf1947b27e033ab9
Silas Sefas <103271018+silassefas1@users.noreply.github.com>
2022-05-14 23:09:35 -0300

    Banco Viajem

--- bancoGeral.py
+++ bancoGeral.py
@@ -3,3 +3,5 @@ banco_Motorista = {44555888990: {"CPF": 44555888990, "nome": "João", "habilitac
 22366895109: {"CPF": 22366895109, "nome": "Paulo", "habilitacao": "B"}}"""
 banco_Veiculo = {'2HBR220': {'placa': "2HBR220", 'tipo': "Carro", "motorista": 'João'},
                  "2HBTR78": {'placa': "2HBTR78", "tipo": "Moto", "motorista": None}}
+banco_Viajem = {'2HBR220':{'Veiculo': veiculo, 'Rota': rota, 'Periodo': periodo, 'Status': status},
+                '2HBTR78':{'Veiculo': veiculo, 'Rota': rota, 'Periodo': periodo, 'Status': status}}
